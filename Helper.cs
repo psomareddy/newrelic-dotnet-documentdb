56e10747f022f66fdfbfe62d9299db480a311970
preddy <preddy@newrelic.com>
2021-07-18 01:12:11 +0000

    first commit

---
+++ Helper.cs
@@ -0,0 +1,56 @@
+﻿using NewRelic.Reflection;
+using System;
+using NewRelic.Agent.Extensions.Parsing;
+
+namespace Custom.Providers.Wrapper.DocumentDb
+{
+    public class Helper
+    {
+        private const string AssemblyName = "Microsoft.Azure.DocumentDB.Core";
+        private const string DocumentServiceRequestTypeName = "Microsoft.Azure.Documents.DocumentServiceRequest";
+        private const string PropertyOperationType = "OperationType";
+        private const string PropertyResourceType = "ResourceType";
+        private const string PropertyResourceAddress = "ResourceAddress";
+        private const string PropertyDatabaseName = "DatabaseName";
+
+        private const string DocumentClientTypeName = "Microsoft.Azure.Documents.Client.DocumentClient";
+        private const string PropertyServiceEndpoint = "ServiceEndpoint";
+
+        private static Func<object, object> _getOperationType;
+        private static Func<object, object> _getResourceType;
+        private static Func<object, string> _getResourceAddress;
+        private static Func<object, Uri> _getServiceEndpoint;
+        private static Func<object, string> _getDatabaseName;
+
+        public static object GetOperationType(object owner)
+        {
+            var idGetter = _getOperationType ?? (_getOperationType = VisibilityBypasser.Instance.GeneratePropertyAccessor<object>(AssemblyName, DocumentServiceRequestTypeName, PropertyOperationType));
+            return idGetter(owner);
+        }
+
+        public static object GetResourceType(object owner)
+        {
+            var rtGetter = _getResourceType ?? (_getResourceType = VisibilityBypasser.Instance.GeneratePropertyAccessor<object>(AssemblyName, DocumentServiceRequestTypeName, PropertyResourceType));
+            return rtGetter(owner);
+        }
+
+        public static string GetResourceAddress(object owner)
+        {
+            var resAddressGetter = _getResourceAddress ?? (_getResourceAddress = VisibilityBypasser.Instance.GeneratePropertyAccessor<string>(AssemblyName, DocumentServiceRequestTypeName, PropertyResourceAddress));
+            return resAddressGetter(owner);
+        }
+
+        public static string GetDatabaseName(object owner)
+        {
+            var dbNameGetter = _getDatabaseName ?? (_getDatabaseName = VisibilityBypasser.Instance.GeneratePropertyAccessor<string>(AssemblyName, DocumentServiceRequestTypeName, PropertyDatabaseName));
+            return dbNameGetter(owner);
+        }
+
+        public static ConnectionInfo GetConnectionInfo(object owner, string dbName)
+        {
+            var serviceEndpointGetter = _getServiceEndpoint ?? (_getServiceEndpoint = VisibilityBypasser.Instance.GeneratePropertyAccessor<Uri>(AssemblyName, DocumentClientTypeName, PropertyServiceEndpoint));
+            Uri endpoint = serviceEndpointGetter(owner);
+            return new ConnectionInfo(endpoint.Host, $"{ endpoint.Port}", dbName);
+        }
+    }
+}
---
+++ Wrapper.cs
@@ -0,0 +1,93 @@
+﻿using NewRelic.Agent.Api;
+using NewRelic.Agent.Extensions.Parsing;
+using NewRelic.Agent.Extensions.Providers.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom.Providers.Wrapper.DocumentDb
+{
+    public class Wrapper : IWrapper
+    {
+        private const string AssemblyName = "Microsoft.Azure.DocumentDB.Core";
+        private const string TypeName = "Microsoft.Azure.Documents.Client.DocumentClient";
+        private string[] MethodNames = new string[]{
+                "CreateAsync",
+                "UpdateAsync",
+                "ReadAsync",
+                "ReadFeedAsync",
+                "DeleteAsync",
+                "ExecuteProcedureAsync",
+                "ExecuteQueryAsync",
+                "UpsertAsync"
+        };
+        bool IWrapper.IsTransactionRequired => true;
+
+        AfterWrappedMethodDelegate IWrapper.BeforeWrappedMethod(InstrumentedMethodCall instrumentedMethodCall, IAgent agent, ITransaction transaction)
+        {
+            if (instrumentedMethodCall.IsAsync)
+            {
+                transaction.AttachToAsync();
+            }
+            var invocationTarget = instrumentedMethodCall.MethodCall.InvocationTarget;
+
+            var model = "unknown";
+            var operation = instrumentedMethodCall.MethodCall.Method.MethodName;
+            string commandText = null;
+
+            ConnectionInfo connectionInfo = null;
+
+            if (instrumentedMethodCall.MethodCall.MethodArguments.Length > 0)
+            {
+                var parm = instrumentedMethodCall.MethodCall.MethodArguments[0];
+                string resourceAddress = Helper.GetResourceAddress(parm);
+                object resourceType = Helper.GetResourceType(parm);
+                object operationType = Helper.GetOperationType(parm);
+
+                model = resourceAddress.Split('/').Take(4).Last();
+                operation = $"{operationType}{resourceType}";
+                commandText = $"{operationType} {resourceType} {model}";
+                var dbName = Helper.GetDatabaseName(parm);
+                connectionInfo = Helper.GetConnectionInfo(invocationTarget, dbName);
+            }
+
+            //Console.WriteLine($"Setting model for {operation}: {model}");
+
+            var segment = transaction.StartDatastoreSegment(
+                instrumentedMethodCall.MethodCall,
+                new ParsedSqlStatement(DatastoreVendor.Other, model, operation),
+                connectionInfo: connectionInfo,
+                commandText: commandText,
+                isLeaf: true);
+
+            if (instrumentedMethodCall.IsAsync)
+            {
+                return Delegates.GetAsyncDelegateFor<Task>(agent, segment);
+            }
+            else
+            {
+                void onComplete()
+                {
+
+                }
+                return Delegates.GetDelegateFor(onComplete: onComplete);
+            }
+
+
+        }
+
+        CanWrapResponse IWrapper.CanWrap(InstrumentedMethodInfo instrumentedMethodInfo)
+        {
+            var method = instrumentedMethodInfo.Method;
+            var canWrap = method.MatchesAny(
+                assemblyNames: new[] { AssemblyName },
+                typeNames: new[] { TypeName },
+                methodNames: MethodNames
+            );
+
+            return new CanWrapResponse(canWrap);
+        }
+    }
+}
